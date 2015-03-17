3a541c0a4f776d629732a6946518d88440c6118f
itcocoa <3216730556@qq.com>
2015-03-17 10:55:48 +0800

    配置测试站点数据库，开启错误消息显示和运行跟踪。

--- new_weixin/Conf/db.php
+++ new_weixin/Conf/db.php
@@ -3,9 +3,9 @@ return array (
   'DB_TYPE' => 'mysql',
   'DB_HOST' => 'localhost',
   'DB_PORT' => '3306',
-  'DB_NAME' => 'weixinpg',
-  'DB_USER' => 'pgweixin',
-  'DB_PWD' => 'CwcTh3SvGsaMLVjd',
+  'DB_NAME' => 'weixin_test',
+  'DB_USER' => 'root',
+  'DB_PWD' => 'LIZEWEI.and.puge',
   'DB_PREFIX' => 'zfwx_',
 );
 ?>
