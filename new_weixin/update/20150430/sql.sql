65c1657b814d7002f6b9a4568eee83fb18e684df
SandraRue <550831182@qq.com>
2015-04-30 14:27:37 +0800

    2015/4/30模板数据库

--- new_weixin/update/20150430/sql.sql
+++ new_weixin/update/20150430/sql.sql
@@ -1,2 +1,6 @@
 /*插入小区模板2*/
 INSERT INTO `zfwx_tmpls` (`id`, `tplid`, `name`, `views`, `type`, `imgurl`, `info`, `attr`, `status`, `sort`) VALUES (NULL, 'xq2', '我的小区模块2', 'xq2_index', '1', 'xq_cate2.png', '我的小区模板2', 'myxq', '1', '138');
+/*插入列表模板8与列表模板9*/
+INSERT INTO `zfwx_tmpls` (`id`, `tplid`, `name`, `views`, `type`, `imgurl`, `info`, `attr`, `status`, `sort`) VALUES
+(201, '8', '列表8', 'list8', 2, 'tihuanlist-2.png', '列表8', '', 1, 255),
+(202, '9', '列表9', 'list9', 2, 'tihuanlist-5.png', '列表9', '', 1, 255);
