467ab6f3e5246ac376d51dddb379fc8dbd59a6e1
SandraRue <550831182@qq.com>
2015-05-26 18:01:08 +0800

    模板201（修改模板33）

---
+++ new_weixin/update/20150526/sql.sql
@@ -0,0 +1,4 @@
+
+/*插入模板201（类似模板33）*/
+INSERT INTO `zfwx_tmpls` (`id`, `tplid`, `name`, `views`, `type`, `imgurl`, `info`, `attr`, `status`, `sort`) VALUES (NULL, '201', '模板201', '201_index_wis2a', '1', 'cate201.png', '此模板支持二级分类，适合分类比较多的地方公众号，小图标为正方形300x300px。', 'sub focu bgs thumb', '1', '255');
+
