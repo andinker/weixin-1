32d8152caa8b466ce8fd8c7256ad5670691efc40
itcocoa <3216730556@qq.com>
2015-08-11 17:31:11 +0800

    商家入驻流程：协议阅读和帐号注册

---
+++ new_weixin/zhifeng/Lib/Action/Home/RegisterAction.class.php
@@ -0,0 +1,39 @@
+<?php
+class RegisterAction extends BaseAction{
+	
+	/**
+	 * 阅读协议
+	 */
+	public function indexAction() {
+		$this->display();
+	}
+	
+	/**
+	 * 注册帐号
+	 */
+	public function accountAction() {
+		$this->display();
+	}
+	
+	/**
+	 * 填写必要信息
+	 */
+	public function infomationAction() {
+		;
+	}
+	
+	/**
+	 * 选择社区
+	 */
+	public function communityAction() {
+		;
+	}
+	
+	/**
+	 * 完成注册
+	 */
+	public function doneAction() {
+		;
+	}
+	
+}
